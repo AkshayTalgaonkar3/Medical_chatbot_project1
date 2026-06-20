c622ec396eed130e498ad687a21e6c8a3c75f5b3
AkshayTalgaonkar3 <talgaonkarakshay1@gmail.com>
2026-06-20 19:54:02 +0530

    folder structure

---
+++ template.sh
@@ -0,0 +1,13 @@
+#creating directory
+mkdir -p src
+mkdir -p research
+#creating file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .evn
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully."
